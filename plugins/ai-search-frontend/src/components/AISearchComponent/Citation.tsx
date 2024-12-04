614d607f9b9db6460a6e5398f77353d6e588019b
Adam Drew <addrew@redhat.com>
2024-12-04 13:21:45 -0500

    Citation links open in new tab. Ran formatter

--- plugins/ai-search-frontend/src/components/AISearchComponent/Citation.tsx
+++ plugins/ai-search-frontend/src/components/AISearchComponent/Citation.tsx
@@ -15,7 +15,12 @@ import { Button } from '@patternfly/react-core';
 import { ExternalLinkSquareAltIcon } from '@patternfly/react-icons';
 
 const parseTitle = citation => {
-  const title = citation.metadata?.title || citation.metadata?.title || citation.metadata?.filename || citation.metadata?.full_path || "Untitled Document";
+  const title =
+    citation.metadata?.title ||
+    citation.metadata?.title ||
+    citation.metadata?.filename ||
+    citation.metadata?.full_path ||
+    'Untitled Document';
   // Remove trailing / if present
   if (title.endsWith('\\')) {
     return title.slice(0, -1);
@@ -26,7 +31,7 @@ const parseTitle = citation => {
 const CitationContent = ({ citation, expanded }) => {
   const makeLink = metadata => {
     if (metadata?.citation_url) {
-      return metadata.citation_url
+      return metadata.citation_url;
     } else {
       // deprecated method, where full_path was used to construct citation url
       // return the path prepended by /docs/
@@ -40,7 +45,7 @@ const CitationContent = ({ citation, expanded }) => {
   }
   return (
     <Card>
-      <CardTitle >
+      <CardTitle>
         <Button
           variant="link"
           icon={<ExternalLinkSquareAltIcon />}
@@ -54,7 +59,22 @@ const CitationContent = ({ citation, expanded }) => {
       </CardTitle>
       <CardBody>
         <TextContent>
-          <Markdown style={{marginLeft: '1em'}}>{citation.page_content}</Markdown>
+          <Markdown
+            style={{ marginLeft: '1em' }}
+            options={{
+              overrides: {
+                a: {
+                  component: 'a',
+                  props: {
+                    target: '_blank',
+                    title: 'Note: Relative links are broken and will not work.',
+                  },
+                },
+              },
+            }}
+          >
+            {citation.page_content}
+          </Markdown>
         </TextContent>
       </CardBody>
     </Card>
