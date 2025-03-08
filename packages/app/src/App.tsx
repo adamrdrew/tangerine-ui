14ad8fcb24ff09df089496239b4338536bfa5006
Adam Drew <addrew@redhat.com>
2025-03-08 11:14:38 -0500

    Logo and side bar

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,52 +1,20 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { apiDocsPlugin } from '@backstage/plugin-api-docs';
 import {
   CatalogEntityPage,
-  catalogPlugin,
 } from '@backstage/plugin-catalog';
-import {
-  catalogImportPlugin,
-} from '@backstage/plugin-catalog-import';
-import {  scaffolderPlugin } from '@backstage/plugin-scaffolder';
-import { orgPlugin } from '@backstage/plugin-org';
-import {
-  techdocsPlugin,
-} from '@backstage/plugin-techdocs';
+
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
 import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
 import { Root } from './components/Root';
 
-import {
-  SignInPage,
-} from '@backstage/core-components';
 import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { ConvoFrontendPage } from '@redhatinsights/backstage-plugin-convo-frontend';
 
 const app = createApp({
   apis,
-  bindRoutes({ bind }) {
-    bind(catalogPlugin.externalRoutes, {
-      createComponent: scaffolderPlugin.routes.root,
-      viewTechDoc: techdocsPlugin.routes.docRoot,
-      createFromTemplate: scaffolderPlugin.routes.selectedTemplate,
-    });
-    bind(apiDocsPlugin.externalRoutes, {
-      registerApi: catalogImportPlugin.routes.importPage,
-    });
-    bind(scaffolderPlugin.externalRoutes, {
-      registerComponent: catalogImportPlugin.routes.importPage,
-      viewTechDoc: techdocsPlugin.routes.docRoot,
-    });
-    bind(orgPlugin.externalRoutes, {
-      catalogIndex: catalogPlugin.routes.catalogIndex,
-    });
-  },
-  components: {
-    SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
-  },
 });
 
 const routes = (
